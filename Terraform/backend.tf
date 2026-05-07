7de971394ca4852642292da0db51f6b678ebcfd2
vinod <vinod.s.19.2001@email.com>
2026-05-07 15:44:53 +0530

    updated jenkinsfile8

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "vinod-devops-terraform-state" # Ensure this bucket exists in AWS
+    bucket         = "vinod-devops-terraform-state" 
     key            = "state/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"        # Ensure this table exists in AWS
+    region         = "us-east-1" # <--- MAKE SURE THIS MATCHES YOUR ACTUAL BUCKET REGION
+    dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
 }
