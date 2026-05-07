72c68cdeb229c515c4317a3e4593dc39c0978280
vinod <vinod.s.19.2001@email.com>
2026-05-07 18:07:14 +0530

    updated jenkinsfile16

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,8 +1,10 @@
+# Keep the public_ip output
 output "public_ip" {
   value = aws_instance.node_app_server.public_ip
 }
 
-output "private_key" {
-  value     = tls_private_key.node_app_key.private_key_pem
-  sensitive = true
-}
+# DELETE OR COMMENT OUT THE BLOCK BELOW:
+# output "private_key" {
+#   value     = tls_private_key.node_app_key.private_key_pem
+#   sensitive = true
+# }
