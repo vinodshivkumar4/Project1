affe184e1da7232e8835ec53b64a56debb8df9b9
vinod <vinod.s.19.2001@email.com>
2026-05-07 17:45:49 +0530

    updated jenkinsfile13

---
+++ Terraform/install_docker.sh
@@ -0,0 +1,8 @@
+cat <<EOF > install_docker.sh
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ubuntu
+EOF
